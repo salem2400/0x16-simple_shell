237ce5243cca63c25c4166602fc85d839a634d32
salem2400 <namperone9@gmail.com>
2023-08-17 15:59:42 +0300

    hello im commit from bin dir

---
+++ header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/*libs*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*prototaypes*/
+void execmd(char **argv);
+
+#endif /*HEADER_H*/
