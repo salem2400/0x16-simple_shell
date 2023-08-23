aece266df1ede60f822a7f806c959d69293e5dc9
group12343 <onegroup935@gmail.com>
2023-08-23 15:59:20 -0700

    creat read and pars line files

---
+++ read_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * read_line - read a line from stdin
+ *
+ * Return: pointer points to str
+ */
+
+char *read_line(void)
+{
+	ssize_t bsize = 0;
+	char *line = NULL;
+
+	if (getline(&line, &bsize, stdin) == -1)
+	{
+		if (feof(stdin))
+			exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		perror("resdline");
+		exit(EXIT_FAILURE);
+	}
+	return (line);
+}
---
+++ spilt_line.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * split_line - split the line input
+ * @line: string to be split
+ *
+ * Return: the tokens
+ */
+
+char **split_line(char *line)
+{
+	int bsize = 64;
+	int c = 0;
+	char **toks;
+	char *tok;
+	char *dlim = " \t\r\n\a"
+
+	toks = malloc(sizeof(char *) * bsize);
+	if (!toks)
+		fprintf(stderr, "allocation error\n");
+		exit(EXIT_FAILURE);
+	tok = strtok(line, dlim);
+	while (tok != NULL)
+		toks[c] = tok;
+		c++;
+		if (c >= bsize)
+			bsize += bsize;
+			toks = realloc(toks, sizeof(char *) * bsize);
+			if (!toks)
+				fprintf(stderr, "realloction error\n");
+				exit(EXIT_FAILURE);
+		tok = strtok(NULL, dlim);
+	toks[c] = NULL;
+	return (toks);
+}
+
+
