f58534253a9f493d87333a4f9bcf0291bf347c18
group12343 <onegroup935@gmail.com>
2023-08-23 18:17:55 -0700

    editing

--- builtin_cmd.c
+++ builtin_cmd.c
@@ -1,33 +1,30 @@
 #include "main.h"
 /**
- * execute - funtion eecute the commend
- *@arguments: have the cmd
+ * num_bulitin - function to calc the size of bulitin
  *
- *Return: the commend to the launch func
+ * Return: num of size bulitin
  */
-char *bulitin[] = {
-	"cd",
-	"help",
-	"exit"
-};
-int (*bulitin_func[])(char **) = {
-	&my_cd,
-	&my_help,
-	&my_exit
-};
-int num_bulitin()
+int num_bulitin(void)
 {
 	return (sizeof(bulitin) / sizeof(char *));
 }
-int execute(char**arguments)
+/**
+ * execute - funcrion that ece the command
+ * @arguments: have the command
+ *
+ * Return: lanch function
+ */
+int execute(char **arguments)
 {
-	int c ;
+	int (*bulitin_func[])(char **) = {&my_cd, &my_help, &my_exit};
+	char *bulitin[] = {"cd", "help", "exit"};
+	int c;
 
 	if (arguments[0] == NULL)
 		return (1);
 	for (c = 0 ; c < num_bulitin() ; c++)
 		if (strcmp(args[0], bulitin[c]) == 0)
-			returm (*bulitin_func[c](arguments));
+			return ((*bulitin_func[c])(arguments));
 	return (launch(arguments));
 }
 /**
@@ -41,15 +38,15 @@ int my_cd(char **arguments)
 	if (arguments[1] == NULL)
 		fprintf(stderr, "expected argument to \'cd\"\n");
 	else
-		if(chdir(arguments[1] != 0))
+		if (chdir(arguments[1] != 0))
 			perror("error");
 	return (1);
 }
 /**
- * my-help - my command help
+ * my_help - my command help
  *@arguments: have the command
  *
- * Retuen: 1 on success
+ * Return: 1 on success
  */
 int my_help(char **arguments)
 {
@@ -65,7 +62,7 @@ int my_help(char **arguments)
 	return (1);
 }
 /**
- * my_exit - function have my exit command 
+ * my_exit - function have my exit command
  * @arguments: have the exit command
  *
  * Return: 0 on exting
--- main.h
+++ main.h
@@ -4,5 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+char *read_line(void);
+char **pars_line(char *line);
+int launch(char **arguments);
+int my_cd(char **arguments);
+int my_help(char **arguments);
+int my_exit(char **arguments);
+int num_bulitin(void);
+int execute(char **arguments);
+void loop(void);
+char *get_location(char *cmd);
+extern char *bulitin[];
+extern int (*bulitin_func[])(char **);
 
 #endif
