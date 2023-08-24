f03c1a224ff63654d02f65f41aa5306ab4e87657
salem2400 <namperone9@gmail.com>
2023-08-24 04:32:24 +0300

    hello im commit from bin dir

---
+++ git_location.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_location - finde the PAT
+ * @cmd: have the command
+ *
+ *Return: null
+ */
+char *get_location(char *cmd)
+{
+	char *dilm = ":";
+	char *path = _getenv("PATH");
+	char *directory = strtok(path, dilm);
+	char *command_path = NULL;
+
+	while (directory != NULL)
+		command_path = malloc(strlen(directory) + strlen(cmd) + 2);
+		sprint(command_path, "%s/%s", directory, cmd);
+		if (access(command_path, X_OK) == 0)
+			return (command_path);
+		free(command_path);
+		directory = strtok(NULL, dilm);
+	return (NULL);
+}
