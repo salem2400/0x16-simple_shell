e094db70acb981c82007af73c96d11a6de9059eb
group12343 <onegroup935@gmail.com>
2023-08-24 15:37:27 -0700

    update

---
+++ non_interactive.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * non_interactive - cheac if in non interactive mode or not
+ *
+ * Return: noting
+ */
+void non_interactive(void)
+{
+	char *line;
+	char **arguments;
+	int stat = -1;
+
+	do {
+		line = read_line();
+		arguments = parse_line(line);
+		stat = execute(arguments);
+		free(line);
+		free(arguments);
+		if (stat >= 0)
+		{
+			exit(stat);
+		}
+	} while (stat == -1);
+}
+
+
---
+++ read_stream.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * read_stream - rad a line feom stream
+ *
+ * Return: pointer that points the read line 
+ */
+char *read_stream(void)
+{
+	int bsize = 1024;
+	int c = 0;
+	char *line;
+	int character;
+
+	line = malloc(sizeof(char) * bsize);
+	if (line == NULL)
+	{
+		fprintf(stderr, "alocation error in read_stream");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		character = getchar();
+		if (character == EFO)
+		{
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
+		else if (character == '\n')
+		{
+			line[c] = '\0';
+			return (line);
+		}
+		else
+		{
+			line[c] = character;
+		}
+		i++;
+		if (c >= bsize)
+		{
+			bsize += bsize;
+			line = realloc(line, bsize);
+			if (line == NULL)
+			{
+				fprintf(stderr, "reallocation error in read_stream");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
