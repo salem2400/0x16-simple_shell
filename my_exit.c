4f1619e2a2e3a96ed5bf58daca8e4432833dbc4e
group12343 <onegroup935@gmail.com>
2023-08-25 18:36:30 -0700

    hello

--- my_exit.c
+++ my_exit.c
@@ -1,78 +1,11 @@
 #include "main.h"
 /**
- **_strncp - copise a srtrig
- * @d: the destination
- * @sr: the sours dtring
- * @n: the amount
+ * my_exit - exit from program
+ * @argument: have the commend
  *
- * Return: the concstensted
+ * Return: 0 in success
  */
-char *_stncpy(char *d, char *sr, int n)
+int my_exit(char *argument)
 {
-	int i;
-	int j;
-	char *s = d;
-
-	i = 0;
-	while (sr[i] != '\0' && i < n-1)
-	{
-		d[i] = sr[i];
-		i++;
-	}
-	if (i < n)
-	{
-		j = i;
-		while (j < n)
-		{
-			d[j] = '\0';
-			j++;
-		}
-	}
-	return (s);
-}
-/**
- **_strncat - concatend toe string
- *@d: the firest
- *@sr: the suocund
- *@n: the amount
- *Return: the concatend
- */
-char *_strncat(char *d, char *sr, int n)
-{
-	int i;
-	int j;
-	char *s = d;
-
-
-	i = 0;
-	j = 0;
-	while (d[i] != '\0')
-	{
-		i++;
-	}
-	while (sr[j] != '\0' && j < n)
-	{
-		d[i] = sr[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-	{
-		d[i] = '\0';
-	}
-	return(s);
-}
-/**
- **_strchr - location a character
- *@sr: the string
- *@count: the character
- *Return: sr  apointer
- */
-char *_strchr(char *sr, char count)
-{
-	do {
-		if (*sr == count)
-			return (sr);
-	} while (*sr++ != '\0');
-	return (NULL);
+	return (0);
 }
