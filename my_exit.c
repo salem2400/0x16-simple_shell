827f850313581a16107aec2dc734e85140e5b423
group12343 <onegroup935@gmail.com>
2023-08-25 18:00:39 -0700

    updating

--- my_exit.c
+++ my_exit.c
@@ -1,20 +1,78 @@
 #include "main.h"
 /**
- * my_exit - function exiting fromm shell
- * @arguments: have the command
+ **_strncp - copise a srtrig
+ * @d: the destination
+ * @sr: the sours dtring
+ * @n: the amount
  *
- * Return: 0 on exiting
+ * Return: the concstensted
  */
-
-int my_exit(char **arguments)
+char *_stncpy(char *d, char *sr, int n)
 {
-	if (arguments[1])
+	int i;
+	int j;
+	char *s = d;
+
+	int i = 0;
+	while (sr[i] != '\0' && i < n-1)
 	{
-		return (atoi(arguments[1]));
+		d[i] = sr[i];
+		i++;
 	}
-	else
+	if (i < n)
 	{
-		return(errno);
+		j = i;
+		while (j < n)
+		{
+			d[j] = '\0';
+			j++
+		}
 	}
-	(void)(arguments);
+	return (s);
+}
+/**
+ **_strncat - concatend toe string
+ *@d: the firest
+ *@sr: the suocund
+ *@n: the amount
+ *Return: the concatend
+ */
+char *_strncat(char *d, char *sr, int n)
+{
+	int i;
+	int j;
+	char *s = d;
+
+
+	i = 0;
+	j = 0;
+	while (d[i] != '\0')
+	{
+		i++;
+	}
+	while (str[j] != '\0' && j < n)
+	{
+		d[i] = sr[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+	{
+		d[i] = '\0';
+	}
+	return(s);
+}
+/**
+ **_strchr - location a character
+ *@sr: the string
+ *@count: the character
+ *Return: sr  apointer
+ */
+char *_strchr(char *sr, char count)
+{
+	do {
+		if (*sr == count)
+			return (sr);
+	} while (*sr++ != '\0');
+	return (NULL);
 }
